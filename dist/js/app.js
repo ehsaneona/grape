ed1327ef4ff2e4a182b7441470c4cfb26755245e
Ehsan Akbarzadeh <ehsaneona@gmail.com>
2024-03-14 01:23:40 +0330

    WIP

--- dist/js/app.js
+++
@@ -1,1 +0,0 @@
-tailwind.config={theme:{extend:{colors:{white:"#fff",black:{900:"#06020C",800:"#0F081D"},purple:{800:"#2E1D41",700:"#270B27"},blue:{900:"#4A4BA6"},pink:{900:"#E547B6"},green:{900:"#01AEA5"},teal:{900:"#20CD8F"},gray:{900:"#d9d9d9"}},backgroundImage:{pattern:"url('/img/pattern.png')"}}}};
